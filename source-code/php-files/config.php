8359b81225d5b7afd96e0b7dae19ca3eae35f9bd
rahulpandharkar <103379268+rahulpandharkar@users.noreply.github.com>
2024-05-13 06:02:33 +0530

    Add files via upload

---
+++ source-code/php-files/config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "user_management_system";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
